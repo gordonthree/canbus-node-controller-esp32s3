7586d972af8705cf4c85bbdf12a3d619e6b5dc9c
Gordon McLellan <gordonthree@gmail.com>
2026-02-09 23:17:22 -0500

    latest version installed

--- Node Controller/src/canbus_flags.h
+++ Node Controller/src/canbus_flags.h
@@ -2,11 +2,12 @@
 #define CAN_FLAGS_H
 //
 
-uint8_t FLAG_SEND_INTRODUCTION = false;    // stop introduction messages
-uint8_t FLAG_BEGIN_NORMAL_OPER = false;    // being normal operation
-uint8_t FLAG_HALT_NORMAL_OPER = false ;    // halt normal operation
-uint8_t FLAG_SEND_HEALTHCHECK = false ;    // send diagnostic health information
-uint8_t FLAG_SEND_NODECHECK = false   ;    // send node if and boot timestamp
+uint8_t FLAG_SEND_INTRODUCTION;    // stop introduction messages
+uint8_t FLAG_BEGIN_NORMAL_OPER;    // being normal operation
+uint8_t FLAG_HALT_NORMAL_OPER ;    // halt normal operation
+uint8_t FLAG_SEND_HEALTHCHECK ;    // send diagnostic health information
+uint8_t FLAG_SEND_NODECHECK   ;    // send node if and boot timestamp
+uint8_t FLAG_PRINT_TIMESTAMP  ;    // print timestamp in human readable format
 
 //
 #endif // end can_flags.h
